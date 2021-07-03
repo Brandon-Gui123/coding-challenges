d179fd8b34d4eb4412f5f0aafd1d5fe9167deb26
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-03 16:04:33 +0800

    Create function in my_solution.cpp
    The function currently does nothing but return a 0.
    I do this so that I can run this function in the "main" method and
    assert the output it provides.

---
+++ FindTheNextPerfectSquare/my_solution.cpp
@@ -0,0 +1,7 @@
+
+int get_next_perfect_square(int square_number)
+{
+    // A perfect square is an integer that is the square of an integer.
+    // So now we know we're dealing with just integers.
+    return 0;
+}
