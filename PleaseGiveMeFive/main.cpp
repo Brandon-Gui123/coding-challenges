2bcbd9a26bfd41809c639b55b7433756edca6726
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-03 23:53:54 +0800

    Add assert to main method
    To check output against the sample results in the challenge post.

--- PleaseGiveMeFive/main.cpp
+++ PleaseGiveMeFive/main.cpp
@@ -1,8 +1,14 @@
+#include <cassert>      // for assert()
 #include <iostream>     // for console
+
+// for the solution I wrote
+#include "my_solution.h"
 
 int main()
 {
-    // TODO Your code logic here
+    assert(contains5(51) == true);
+    assert(contains5(200) == false);
+    assert(contains5(555) == true);
     
     return 0;
 }
