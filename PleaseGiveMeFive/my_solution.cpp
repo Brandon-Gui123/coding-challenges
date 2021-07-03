3e00d7c321a717bd8c1fc93474aed330454b3f97
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-04 00:33:42 +0800

    Mark local vars as const
    Won't be changing them so would be good to mark it as const.

--- PleaseGiveMeFive/my_solution.cpp
+++ PleaseGiveMeFive/my_solution.cpp
@@ -6,12 +6,12 @@ bool contains5(int number_to_check)
     // would be to convert the integer
     // to a string, and then searching for the
     // character '5'
-    std::string num_as_string{ std::to_string(number_to_check) };
+    const std::string num_as_string{ std::to_string(number_to_check) };
 
     // the result is the position of the found character
     // or std::string::npos if no such character is found
     // see: https://en.cppreference.com/w/cpp/string/basic_string/find_first_of
-    size_t pos{ num_as_string.find_first_of('5') };
+    const size_t pos{ num_as_string.find_first_of('5') };
 
     return pos != std::string::npos;
 }
