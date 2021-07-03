b43e1485b3499f133cbe4368f1a53b1710daf515
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-04 01:52:54 +0800

    Add assertions to main method
    To make it absolutely clear that the type is std::string, I declared
    variables to hold the input and expected output.
    Also, I do not intend to have implicit conversions get in the way, so by
    using the type directly, I do not have to worry about any side effects.

--- BuildASimpleEncoder/main.cpp
+++ BuildASimpleEncoder/main.cpp
@@ -1,8 +1,23 @@
+#include <cassert>      // for assert()
 #include <iostream>     // for console
+#include <string>       // for std::string
+
+// for the solution I wrote
+#include "my_solution.h"
 
 int main()
 {
-    // TODO Your code logic here
+    const std::string firstInput{ "abc" };
+    const std::string secondInput{ "zoo" };
+    const std::string thirdInput{ "Alan" };
+
+    const std::string firstOutput{ "123" };
+    const std::string secondOutput{ "261515" };
+    const std::string thirdOutput{ "112114" };
+
+    assert(replaceCharWithAlphabetPositions(firstInput) == firstOutput);
+    assert(replaceCharWithAlphabetPositions(secondInput) == secondOutput);
+    assert(replaceCharWithAlphabetPositions(thirdInput) == thirdOutput);
     
     return 0;
 }
