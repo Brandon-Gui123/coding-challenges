efa4eec6e0607e2e36f324ca8467e8b0585632d7
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-04 02:21:44 +0800

    Move unordered map to new header file
    Having the unordered map in the my_solution header file seems to be
    causing problems, so I moved it out to another header file.
    
    Problem related to having one or more symbols multiply defined
    (according to VS 2019).

---
+++ BuildASimpleEncoder/constants.h
@@ -0,0 +1,16 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <unordered_map>    // for std::unordered_map
+
+std::unordered_map<char, int> alphabetPositions
+{
+    { 'a', 1 }, { 'b', 2 }, { 'c', 3 }, { 'd', 4 }, { 'e', 5 },
+    { 'f', 6 }, { 'g', 7 }, { 'h', 8 }, { 'i', 9 }, { 'j', 10 },
+    { 'k', 11 }, { 'l', 12 }, { 'm', 13 }, { 'n', 14 }, { 'o', 15 },
+    { 'p', 16 }, { 'q', 17 }, { 'r', 18 }, { 's', 19 }, { 't', 20 },
+    { 'u', 21 }, { 'v', 22 }, { 'w', 23 }, { 'x', 24 }, { 'y', 25 },
+    { 'z', 26 }
+};
+
+#endif  // !CONSTANTS_H
