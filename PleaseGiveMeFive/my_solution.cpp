9dacd30f78835ac1613afd79d4e3669bf57ea355
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-04 01:15:32 +0800

    Add implementation for alt solution

--- PleaseGiveMeFive/my_solution.cpp
+++ PleaseGiveMeFive/my_solution.cpp
@@ -18,5 +18,30 @@ bool contains5(int number_to_check)
 
 bool contains5_alternative(int number_to_check)
 {
-    throw "Unimplemented function!";
+    // Another solution that doesn't make use of
+    // conversions to strings
+    // 1. Modulo the arg by 10 to get the rightmost digit
+    // 2. Check the first digit to see if it is a 5
+    // 3. If it is, return true.
+    // 4. If not, divide arg by 10 to remove the first digit
+    // 5. Repeat until arg is 0.
+
+    int number_to_check_copy{ number_to_check };
+
+    while (number_to_check_copy > 0)
+    {
+        int rightmost{ number_to_check_copy % 10 };
+
+        if (rightmost == 5)
+        {
+            return true;
+        }
+        else
+        {
+            number_to_check_copy /= 10;
+        }
+    }
+
+    // if we're here, it means we can't find any 5s
+    return false;
 }
