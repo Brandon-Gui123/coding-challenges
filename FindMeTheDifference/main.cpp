0b1edfe4fac7967d82e6f84c5a244e3081025708
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-05 12:30:40 +0800

    Create assertion in main function

--- FindMeTheDifference/main.cpp
+++ FindMeTheDifference/main.cpp
@@ -1,8 +1,21 @@
+// for my solution
+#include "my_solution.h"
+
+#include <cassert>      // for assert()
 #include <iostream>     // for console
+#include <vector>       // for std::vector
+#include <string>       // for std::string
 
 int main()
 {
-    // TODO Your code logic here
+    std::vector<std::string> strings
+    {
+        "ProgrammerPlus",
+        "empowers",
+        "you"
+    };
+
+    assert(getDifferenceBetweenLongestAndShortest(strings) == 11);
     
     return 0;
 }
