485bf653a20b20daba09ed6caa1b7e067e1a117e
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-05 16:23:44 +0800

    Create assertions in main function

--- DigitalItUpALittleBit/main.cpp
+++ DigitalItUpALittleBit/main.cpp
@@ -1,8 +1,14 @@
+// for the solutions I wrote
+#include "my_solution.h"
+
+#include <cassert>      // for assert()
 #include <iostream>     // for console
 
 int main()
 {
-    // TODO Your code logic here
+    assert(getNumberOfOnesInBinary(32) == 1);
+    assert(getNumberOfOnesInBinary(31) == 5);
+    assert(getNumberOfOnesInBinary(5) == 2);
     
     return 0;
 }
