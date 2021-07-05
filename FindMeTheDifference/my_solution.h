ffea178c37b5245ac82600cfec91d57ef3e4eaac
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-05 12:57:16 +0800

    Create function declaration in header file

--- FindMeTheDifference/my_solution.h
+++ FindMeTheDifference/my_solution.h
@@ -1,9 +1,14 @@
 #ifndef MY_SOLUTION_H
 #define MY_SOLUTION_H
 
+#include <cstddef>  // for std::size_t
+#include <array>    // for std::array
 #include <string>   // for std::string
 #include <vector>   // for std::vector
 
 int getDifferenceBetweenLongestAndShortest(const std::vector<std::string>& strings);
+
+template <std::size_t size>
+int getDifferenceBetweenLongestAndShortest(const std::array<std::string, size>& strings);
 
 #endif  // !MY_SOLUTION_H
