aa8e3c1720d18d217167da6f3d51d39cb8d09a12
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-05 22:45:35 +0800

    Create unimplemented functions

---
+++ Coding Challenges 2/EnglishTextToMorseCode/my_solution.cpp
@@ -0,0 +1,11 @@
+#include <string>   // for std::string
+
+std::string stringToMorseCode(const std::string& string)
+{
+    throw "Unimplemented function";
+}
+
+std::string morseCodeToString(const std::string& morseCode)
+{
+    throw "Unimplemented function";
+}
