3e9d39230097d1e56b53895814e92eb1e0e4b0bc
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-05 12:27:23 +0800

    Create unimplemented function
    The function is made to throw a string when used so
    that it makes it clear that this function isn't implemented.
    
    This will be useful when I write assertions for this function later.

---
+++ FindMeTheDifference/my_solution.cpp
@@ -0,0 +1,8 @@
+
+#include <string>   // for std::string
+#include <vector>   // for std::vector
+
+int getDifferenceBetweenLongestAndShortest(const std::vector<std::string>& strings)
+{
+    throw "Function not implemented!";
+}
