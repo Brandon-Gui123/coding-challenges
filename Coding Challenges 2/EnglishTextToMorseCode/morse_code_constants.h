ad28054e7b23439781adbc3686e4d5cb298fb0e0
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-06 17:22:35 +0800

    Create compile-time constants

--- Coding Challenges 2/EnglishTextToMorseCode/morse_code_constants.h
+++ Coding Challenges 2/EnglishTextToMorseCode/morse_code_constants.h
@@ -1,4 +1,10 @@
 #ifndef MORSE_CODE_CONSTANTS_H
 #define MORSE_CODE_CONSTANTS_H
 
+namespace MorseCodeConstants
+{
+    constexpr char dot{ '.' };
+    constexpr char dash{ '-' };
+}
+
 #endif  // !MORSE_CODE_CONSTANTS_H
