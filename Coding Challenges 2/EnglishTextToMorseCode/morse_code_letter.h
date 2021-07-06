4541845c4c72e9097e68f4f4af1df5800c2e37be
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-06 15:32:03 +0800

    Create MorseCodeLetter struct

---
+++ Coding Challenges 2/EnglishTextToMorseCode/morse_code_letter.h
@@ -0,0 +1,12 @@
+#ifndef MORSE_CODE_LETTER_H
+#define MORSE_CODE_LETTER_H
+
+#include <string>       // for std::string
+
+struct MorseCodeLetter
+{
+    char letter;
+    std::string morseCode;
+};
+
+#endif  // !MORSE_CODE_LETTER_H
