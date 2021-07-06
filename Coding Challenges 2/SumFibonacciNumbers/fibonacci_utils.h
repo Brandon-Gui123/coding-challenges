ff80c6f7787b3daab384e8f321800e60712181d7
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-06 21:13:44 +0800

    Create generate function declaration

--- Coding Challenges 2/SumFibonacciNumbers/fibonacci_utils.h
+++ Coding Challenges 2/SumFibonacciNumbers/fibonacci_utils.h
@@ -1,4 +1,11 @@
 #ifndef FIBONACCI_UTILS_H
 #define FIBONACCI_UTILS_H
 
+#include <vector>           // for std::vector
+
+namespace fibonacci
+{
+    std::vector<int> generate(int count);
+}
+
 #endif  // !FIBONACCI_UTILS_H
