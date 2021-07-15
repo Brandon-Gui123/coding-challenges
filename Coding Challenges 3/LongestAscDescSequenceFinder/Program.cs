51caffc11558d1a9e81506b768321c96f1322500
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-15 23:35:52 +0800

    Update main method to test

--- Coding Challenges 3/LongestAscDescSequenceFinder/Program.cs
+++ Coding Challenges 3/LongestAscDescSequenceFinder/Program.cs
@@ -1,12 +1,17 @@
 ﻿namespace LongestAscDescSequenceFinder
 {
     using System;
+    using System.Collections.Generic;
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Sequence longestAscending = SequenceUtils.GetLongestAscendingSequenceFromNumber(74682);
+
+            Console.Write("ASC- input: 74682\toutput: ");
+            longestAscending.digits.ForEach(digit => Console.Write(digit));     // because why not? :P
+            Console.WriteLine();
         }
     }
 }
