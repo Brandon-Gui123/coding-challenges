4697a19b8d27ee282e5de8257712a267b36bea62
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-15 22:36:31 +0800

    Create DeconstructToDigits method
    This takes a number and breaks it down into its digits.

--- Coding Challenges 3/LongestAscDescSequenceFinder/SequenceUtils.cs
+++ Coding Challenges 3/LongestAscDescSequenceFinder/SequenceUtils.cs
@@ -1,6 +1,19 @@
 ﻿namespace LongestAscDescSequenceFinder
 {
+    using System.Collections.Generic;
+
     public class SequenceUtils
     {
+        public static List<int> DeconstructToDigits(int number)
+        {
+            List<int> numberDigits = new();
+
+            for (int num = number; num > 0; num /= 10)
+            {
+                numberDigits.Insert(0, num % 10);
+            }
+
+            return numberDigits;
+        }
     }
 }
