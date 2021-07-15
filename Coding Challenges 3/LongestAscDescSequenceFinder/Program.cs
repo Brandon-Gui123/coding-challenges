233a496c5537222d77e9d2679331dff00c073c25
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-15 23:59:43 +0800

    Update main method to print results

--- Coding Challenges 3/LongestAscDescSequenceFinder/Program.cs
+++ Coding Challenges 3/LongestAscDescSequenceFinder/Program.cs
@@ -7,10 +7,15 @@
     {
         static void Main(string[] args)
         {
-            Sequence longestAscending = SequenceUtils.GetLongestAscendingSequenceFromNumber(74682);
+            Sequence longestAscending = SequenceUtils.GetLongestSequenceFromNumber(74682, (current, previous) => current >= previous);
+            Sequence longestDescending = SequenceUtils.GetLongestSequenceFromNumber(48642, (current, previous) => current <= previous);
 
             Console.Write("ASC- input: 74682\toutput: ");
             longestAscending.digits.ForEach(digit => Console.Write(digit));     // because why not? :P
+            Console.WriteLine();
+
+            Console.Write("DESC- input: 48642\toutput: ");
+            longestDescending.digits.ForEach(digit => Console.Write(digit));
             Console.WriteLine();
         }
     }
