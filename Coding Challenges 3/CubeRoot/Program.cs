e1c28897cbd0ce282705689568e568911b47f066
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-16 11:51:56 +0800

    Add comments about the solution I found
    Also, this is the first time I did binary search on something.

--- Coding Challenges 3/CubeRoot/Program.cs
+++ Coding Challenges 3/CubeRoot/Program.cs
@@ -1,4 +1,15 @@
-﻿namespace CubeRoot
+﻿/*
+ * I was curious how people do it and found one solution
+ * here: https://www.geeksforgeeks.org/find-cubic-root-of-a-number/
+ *
+ * Basically, it uses binary search to find a number and check
+ * if the cubed of the number is equal to the method argument.
+ *
+ * Here, I've re-written it with comments and a default argument
+ * for the epsilon.
+ */
+
+namespace CubeRoot
 {
     using System;
 
