b8dc39af863e0269abe3f80d965841d77a52ea2a
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-16 23:37:55 +0800

    Create array of strings for 10 to 19
    Created an array of strings containing the names for numbers 10 to 19.

--- Coding Challenges 4/NumberToWord/src/main/java/NumberToWord.java
+++ Coding Challenges 4/NumberToWord/src/main/java/NumberToWord.java
@@ -14,6 +14,20 @@ public class NumberToWord
             "nine"
     };
 
+    private static final String[] TEN_TO_NINETEEN_NAMES =
+    {
+            "ten",
+            "eleven",
+            "twelve",
+            "thirteen",
+            "fourteen",
+            "fifteen",
+            "sixteen",
+            "seventeen",
+            "eighteen",
+            "nineteen"
+    };
+
     public static String convert(int number)
     {
         return SINGLE_DIGIT_NAMES[number];
