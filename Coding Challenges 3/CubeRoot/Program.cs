65fb30276ff57066c81d28bb808af149cb695f7a
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-16 10:55:35 +0800

    Create project to do the challenge

---
+++ Coding Challenges 3/CubeRoot/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CubeRoot
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
