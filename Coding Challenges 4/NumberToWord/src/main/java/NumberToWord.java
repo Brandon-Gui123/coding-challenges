5f77b64f2053f78f640b1529a919fca260432d18
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-16 23:06:36 +0800

    Create array for names of single digits

--- Coding Challenges 4/NumberToWord/src/main/java/NumberToWord.java
+++ Coding Challenges 4/NumberToWord/src/main/java/NumberToWord.java
@@ -1,3 +1,16 @@
 public class NumberToWord
 {
+    private static final String[] SINGLE_DIGIT_NAMES =
+    {
+            "zero",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine"
+    };
 }
