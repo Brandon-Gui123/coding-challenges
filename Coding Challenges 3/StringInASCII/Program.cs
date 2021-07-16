e75391872ebd6f1b9e939fdeeb8db59ac5d1459a
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-16 09:45:00 +0800

    Create ToASCII method

--- Coding Challenges 3/StringInASCII/Program.cs
+++ Coding Challenges 3/StringInASCII/Program.cs
@@ -1,12 +1,26 @@
 ﻿namespace StringInASCII
 {
     using System;
+    using System.Text;
 
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static string ToASCII(string str)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(str);
+
+            string ascii = "";
+            foreach (byte b in bytes)
+            {
+                ascii += b.ToString();
+            }
+
+            return ascii;
         }
     }
 }
