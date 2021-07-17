da00365e1dfca1c46f48569ad27a480983c9837b
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-07-18 00:52:42 +0800

    Add print to match results in main
    Forgot to do that to show that what I got is the same as the
    challenge's sample outputs.

--- Coding Challenges 4/NumberToWord/src/main/java/ProgramMain.java
+++ Coding Challenges 4/NumberToWord/src/main/java/ProgramMain.java
@@ -2,6 +2,7 @@ public class ProgramMain
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello there!");
+        System.out.println("12 : " + NumberToWord.convert(12));
+        System.out.println("463 : " + NumberToWord.convert(463));
     }
 }
