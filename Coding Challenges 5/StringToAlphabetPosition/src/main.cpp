7789556568e1189ed90276a0916402d6e94b36d2
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-12 12:01:56 +0800

    Implement solution in main.cpp
    This is the solution that I can think of in the shortest duration.

--- Coding Challenges 5/StringToAlphabetPosition/src/main.cpp
+++ Coding Challenges 5/StringToAlphabetPosition/src/main.cpp
@@ -1,7 +1,51 @@
+#include <cctype>
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello, world!\n";
+    // use the string example shown in the post
+    std::string example{ "The sunset sets at twelve o' clock." };
+
+    // remove all the characters that aren't alphabets
+    std::string onlyLetters{};
+    for (char letter : example)
+    {
+        if (std::isalpha(letter))
+        {
+            onlyLetters.push_back(letter);
+        }
+    }
+
+    // convert all uppercase characters to lowercase
+    std::string lowercaseLetters{};
+    for (char letter : onlyLetters)
+    {
+        lowercaseLetters.push_back(std::tolower(letter));
+    }
+
+    // each element in the string is a character
+    // we can use the ASCII table since each character
+    // is mapped onto that
+    // we subtract the value of the backtick char '`'
+    // from the character to get a number corresponding
+    // to the letter in the alphabet
+    std::string alphabetPositionings{};
+    for (char letter : lowercaseLetters)
+    {
+        int letterPosition{ letter - '`' };
+        alphabetPositionings += std::to_string(letterPosition);
+
+        // only add a space if the current letter isn't the last
+        if (letter != *lowercaseLetters.rend())
+        {
+            alphabetPositionings.push_back(' ');
+        }
+    }
+
+    // print results
+    std::cout << "In: " << example << '\n';
+    std::cout << "Out: " << alphabetPositionings << '\n';
+
     return 0;
 }
