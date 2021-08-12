48a1eafbfbda6309570abdc21c988c850d258817
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-12 20:35:25 +0800

    Create remove_non_alpha forward declaration

--- Coding Challenges 5/StringToAlphabetPosition/src/my_utils.h
+++ Coding Challenges 5/StringToAlphabetPosition/src/my_utils.h
@@ -1,9 +1,11 @@
 #ifndef MY_UTILS_H
 #define MY_UTILS_H
 
+#include <string>       // for std::string
+
 namespace my_utils
 {
-    
+    std::string remove_non_alpha(const std::string& str);
 }
 
 #endif // !MY_UTILS_H
