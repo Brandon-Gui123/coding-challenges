8e5790a43b2e91bc355b3eeb4017e48fd541032d
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-12 21:53:35 +0800

    Create get_letter_pos forward declaration

--- Coding Challenges 5/StringToAlphabetPosition/src/my_utils.h
+++ Coding Challenges 5/StringToAlphabetPosition/src/my_utils.h
@@ -8,6 +8,8 @@ namespace my_utils
     std::string remove_non_alpha(const std::string& str);
 
     void to_lowercase(std::string& strToModify);
+
+    int get_letter_pos(char ch);
 }
 
 #endif // !MY_UTILS_H
