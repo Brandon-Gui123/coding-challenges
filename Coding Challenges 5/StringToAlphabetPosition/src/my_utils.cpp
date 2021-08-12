79ddef1e6fd6f4ce0869a072f6dfc0ea193c4793
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-12 20:38:08 +0800

    Create implementation for remove_non_alpha

--- Coding Challenges 5/StringToAlphabetPosition/src/my_utils.cpp
+++ Coding Challenges 5/StringToAlphabetPosition/src/my_utils.cpp
@@ -1,4 +1,20 @@
+#include <cctype>       // for std::isalpha
+#include <string>       // for std::string
+
 namespace my_utils
 {
-    
+    std::string remove_non_alpha(const std::string& str)
+    {
+        std::string modifiedString{};
+
+        for (char ch : str)
+        {
+            if (std::isalpha(ch))
+            {
+                modifiedString.push_back(ch);
+            }
+        }
+
+        return modifiedString;
+    }
 }
