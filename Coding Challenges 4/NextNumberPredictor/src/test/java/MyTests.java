bcb8f82ec5825ab670e77821854ff329d2dbc654
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-19 13:24:37 +0800

    Create test for addition number patterns

--- Coding Challenges 4/NextNumberPredictor/src/test/java/MyTests.java
+++ Coding Challenges 4/NextNumberPredictor/src/test/java/MyTests.java
@@ -3,4 +3,13 @@ import org.junit.jupiter.api.Test;
 
 public class MyTests
 {
+    // Test on number patterns where the same number just keeps getting added
+    @Test
+    public void testAdditionPattern()
+    {
+        int[] numberPattern = { 4, 8, 12, 16 };
+        int nextNumber = PatternFinder.findNextNumber(numberPattern);
+
+        Assertions.assertEquals(20, nextNumber);
+    }
 }
