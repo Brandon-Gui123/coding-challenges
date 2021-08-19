0035ad78468d6ce8f1e048672c47be800ac54b9e
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-19 15:45:51 +0800

    Add examples to 'main'
    So we can quickly compare inputs and outputs with what is shown
    on the image.

--- Coding Challenges 4/NextNumberPredictor/src/main/java/Program.java
+++ Coding Challenges 4/NextNumberPredictor/src/main/java/Program.java
@@ -2,6 +2,22 @@ public class Program
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world");
+        int[] numberPattern1 = { 1, 4, 9, 16 };
+
+        for (int num : numberPattern1)
+        {
+            System.out.print(num + " ");
+        }
+        System.out.print(": ");
+        System.out.println(PatternFinder.findNextNumber(numberPattern1));
+
+        int[] numberPattern2 = { 4, 8, 12, 16 };
+
+        for (int num : numberPattern2)
+        {
+            System.out.print(num + " ");
+        }
+        System.out.print(": ");
+        System.out.println(PatternFinder.findNextNumber(numberPattern2));
     }
 }
