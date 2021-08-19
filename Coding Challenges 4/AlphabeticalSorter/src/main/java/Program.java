8b734d21e0797e1cedb4610740fb44252e5d6ddd
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-19 23:45:32 +0800

    Update main method to use new methods

--- Coding Challenges 4/AlphabeticalSorter/src/main/java/Program.java
+++ Coding Challenges 4/AlphabeticalSorter/src/main/java/Program.java
@@ -4,32 +4,11 @@ public class Program
     {
         String sample = "challenge";
         System.out.print(sample + " : ");
+        System.out.println(sortString(sample));
 
-        // we're assuming all inputs are in lowercase
-
-        // convert the input to an array of characters
-        char[] charArr = sample.toCharArray();
-
-        // we'll use bubble sort to sort the characters in terms of order on the ASCII
-        while (!isSorted(charArr))
-        {
-            for (int i = 0; i < charArr.length - 1; i++)
-            {
-                // is the current char more than the char ahead?
-                if (Character.compare(charArr[i], charArr[i + 1]) > 0)
-                {
-                    // the current char needs to be swapped with the char ahead
-                    char currentCopy = charArr[i];
-                    charArr[i] = charArr[i + 1];
-                    charArr[i + 1] = currentCopy;
-                }
-            }
-        }
-
-        // once the array is sorted, we turn the array of characters to a string
-        String sorted = new String(charArr);
-
-        System.out.println(sorted);
+        String sample2 = "$php7";
+        System.out.print(sample2 + " : ");
+        System.out.println(sortString(sample2));
     }
 
     public static boolean isSorted(char[] charArr)
