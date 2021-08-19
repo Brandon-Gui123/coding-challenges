fd93b9a2d7108b11d22975692852bdbee9e252cb
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-19 15:28:52 +0800

    Add implementation for square number patterns

--- Coding Challenges 4/NextNumberPredictor/src/main/java/PatternFinder.java
+++ Coding Challenges 4/NextNumberPredictor/src/main/java/PatternFinder.java
@@ -4,6 +4,7 @@ public class PatternFinder
     {
         // let's start simple by evaluating addition number patterns
         // we need to calculate the difference between each number
+        boolean isAdditionNumberPattern = true;
 
         // start by calculating the difference between the last 2 numbers in the pattern
         int difference = numberPattern[numberPattern.length - 1] - numberPattern[numberPattern.length - 2];
@@ -17,11 +18,38 @@ public class PatternFinder
             if (numberPattern[i] - numberPattern[i - 1] != difference)
             {
                 System.out.println("This is not an addition number pattern!");
+                isAdditionNumberPattern = false;
                 break;
             }
         }
 
-        // the next number can be predicted by adding the difference to the last element in the array
-        return numberPattern[numberPattern.length - 1] + difference;
+        if (isAdditionNumberPattern)
+        {
+            // the next number can be predicted by adding the difference to the last element in the array
+            return numberPattern[numberPattern.length - 1] + difference;
+        }
+
+        // now let's assume our number pattern contains exponent (n^2 where n is the nth element)
+        // we first check if n, for all numbers in the array, is raised by power of 2 every time
+        // to obtain the power, use log base n
+        // to get to base n, use the change of base formula
+        // we cannot use the first element because log(1) is invalid,
+        // so we use the second element instead
+        double power = Math.log(numberPattern[1]) / Math.log(2);
+
+        for (int i = 2; i < numberPattern.length; i++)
+        {
+            double pow = Math.log(numberPattern[i]) / Math.log(i + 1);
+
+            // inconsistent power
+            if (pow != power)
+            {
+                System.out.println("Inconsistent power!");
+                break;
+            }
+        }
+
+        // our next number is the length of the array + 1, raised to the calculated power
+        return (int)Math.pow(numberPattern.length + 1, power);
     }
 }
