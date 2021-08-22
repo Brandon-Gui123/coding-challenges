b716c4bf5b0e9e761507a8da7ed4c10ecba19209
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-22 17:30:14 +0800

    Create my_functions source file

---
+++ Coding Challenges 5/GetMissingLetter/src/my_functions.cpp
@@ -0,0 +1,9 @@
+#include <vector>       // for std::vector
+
+namespace my_functions
+{
+    char findMissingLetter(const std::vector<char>& consecutiveLetters)
+    {
+        return 'a';
+    }
+}
