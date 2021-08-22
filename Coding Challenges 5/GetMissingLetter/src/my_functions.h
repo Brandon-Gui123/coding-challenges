fd2859809e214acd3631fe19b9482cda8431853d
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-22 17:29:53 +0800

    Create my_functions header file

---
+++ Coding Challenges 5/GetMissingLetter/src/my_functions.h
@@ -0,0 +1,11 @@
+#ifndef MY_FUNCTIONS_H
+#define MY_FUNCTIONS_H
+
+#include <vector>       // for std::vector
+
+namespace my_functions
+{
+    char findMissingLetter(const std::vector<char>& consecutiveLetters);
+}
+
+#endif  // !MY_FUNCTIONS_H
