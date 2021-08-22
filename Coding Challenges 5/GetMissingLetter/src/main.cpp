930415811009d277db7eb6762958599a046304b6
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-22 17:43:49 +0800

    Update main to match inputs and outputs of image

--- Coding Challenges 5/GetMissingLetter/src/main.cpp
+++ Coding Challenges 5/GetMissingLetter/src/main.cpp
@@ -1,7 +1,17 @@
+#include "my_functions.h"
+
+#include <cassert>      // for assert() macro
 #include <iostream>
+#include <vector>       // for std::vector
 
 int main()
 {
-    std::cout << "Hello there, world!\n";
+    // let's assume our input is the same as the example provided in the post
+    std::vector<char> exampleInput{ 'a', 'b', 'c', 'd', 'f' };
+
+    char missingChar{ my_functions::findMissingLetter(exampleInput) };
+
+    assert(missingChar == 'e');
+
     return 0;
 }
