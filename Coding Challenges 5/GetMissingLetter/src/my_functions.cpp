c017dc6229194bd0819eed59bafc1909070cf178
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-22 17:53:13 +0800

    Create implementation for findMissingLetter
    This is the solution I can think of in the
    shortest amount of time possible.

--- Coding Challenges 5/GetMissingLetter/src/my_functions.cpp
+++ Coding Challenges 5/GetMissingLetter/src/my_functions.cpp
@@ -4,6 +4,31 @@ namespace my_functions
 {
     char findMissingLetter(const std::vector<char>& consecutiveLetters)
     {
-        return 'a';
+        // requirements: consecutive (increasing letters)
+        // this means the vector is expected to have already been sorted
+
+        // fastest solution I can think of: rely on the ASCII table
+
+        // initialize a variable to hold the value of the first element in the vector
+        char expectedLetter{ consecutiveLetters[0] };
+        
+        // create a 'for' loop, starting at vector's 2nd element, all the way to the end
+        for (int i{ 1 }; i < consecutiveLetters.size(); ++i)
+        {
+            // increment the variable declared above by 1 (++)
+            ++expectedLetter;
+            
+            // compare equality of variable and element at index i
+            if (expectedLetter != consecutiveLetters[i])
+            {
+                // if different, return the expected letter
+                return expectedLetter;
+            }
+        }
+        
+        // we need a sentinel to indicate that there are no missing letters
+        // maybe we can return '0' for now (it isn't even a letter so we should be good)
+
+        return '0';
     }
 }
