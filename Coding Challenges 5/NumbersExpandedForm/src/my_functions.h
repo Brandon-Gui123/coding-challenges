3a212e7aa7083a47dc0cc7c6fdcab32771c0d707
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-23 12:52:29 +0800

    Create function to print vector contents to stdout
    Note that the implementation is defined in the header file for the
    template function because the compiler needs the definition to
    instantiate the function with the type we specify in T.
    
    See https://isocpp.org/wiki/faq/templates#templates-defn-vs-decl

--- Coding Challenges 5/NumbersExpandedForm/src/my_functions.h
+++ Coding Challenges 5/NumbersExpandedForm/src/my_functions.h
@@ -1,9 +1,19 @@
 #ifndef MY_FUNCTIONS_H
 #define MY_FUNCTIONS_H
 
+#include <iostream>     // for std::cout
+#include <vector>       // for std::vector
+
 namespace my_functions
 {
-    
+    template <typename T>
+    void printVectorToStdOut(std::vector<T> v)
+    {
+        for (const auto& el : v)
+        {
+            std::cout << el << '\n';
+        }
+    }
 }
 
 #endif  // !MY_FUNCTIONS_H
