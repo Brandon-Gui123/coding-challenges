dfdbc73d06829c621878edae82b76db428cc6aa0
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-25 12:08:12 +0800

    Create char constants in my_constants.h

--- Coding Challenges 5/NumbersExpandedForm/src/my_constants.h
+++ Coding Challenges 5/NumbersExpandedForm/src/my_constants.h
@@ -1,6 +1,14 @@
 #ifndef MY_CONSTANTS_H
 #define MY_CONSTANTS_H
 
-
+namespace my_constants
+{
+    constexpr char zeroChar{ '0' };
+    constexpr char tenString[]{ "10" };
+    constexpr char divideChar{ '/' };
+    constexpr char multiplyChar{ '*' };
+    constexpr char plusChar{ '+' };
+    constexpr char dotChar{ '.' };
+}
 
 #endif  // !MY_CONSTANTS_H
