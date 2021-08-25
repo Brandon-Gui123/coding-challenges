434dd012c1b81a235e48b7bad7dbc80b50abdafd
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-25 10:34:20 +0800

    Add basic implementation for expansion
    This implementation currently uses the input provided in the example
    shown on the post.

--- Coding Challenges 5/NumbersExpandedForm/src/main.cpp
+++ Coding Challenges 5/NumbersExpandedForm/src/main.cpp
@@ -1,7 +1,136 @@
+#include "my_functions.h"
+
 #include <iostream>
+#include <string>       // for std::string
+#include <vector>       // for std::vector
 
 int main()
 {
-    std::cout << "Hello there, world!\n";
+    // plan
+    // input: double
+    // output: string
+
+    // not a good idea to simply subtract sampleInput with the integral
+    // part to get the decimal parts because floating point errors
+    // may cause a ton of decimals to show up
+    // so to prevent floating point errors, we should convert the input to a string
+
+    double sampleInput{ 101.24 };
+
+    std::string sampleInputAsString{ std::to_string(sampleInput) };
+
+    // to separate the integral and decimal parts, we can loop through all the characters
+    // in the string and first store it in a vector for integral digits
+    // when we encounter a period (the decimal dot), we can set a boolean and this time,
+    // all the numbers we encounter in the loop are going to another vector for decimal digits
+    std::vector<char> integralDigits{};
+    std::vector<char> decimalDigits{};
+
+    bool shouldFillDecimalDigits{ false };
+    for (char digit : sampleInputAsString)
+    {
+        if (digit == '.')
+        {
+            shouldFillDecimalDigits = true;
+
+            // to skip the decimal dot
+            continue;
+        }
+
+        if (shouldFillDecimalDigits)
+        {
+            decimalDigits.push_back(digit);
+        }
+        else
+        {
+            integralDigits.push_back(digit);
+        }
+    }
+
+    // the leftmost digit of the input is the first element in the integralDigits vector
+    // and also, this digit has the highest place value.
+    // to easily deal with this, we can use 'rbegin()' and 'rend()' to instead start
+    // from the last element, which is the digit with the lowest place value
+    // then as we iterate, we add a '0' to the end of the "10"
+    std::string multiplyValue{ "10" };
+    std::vector<std::string> expandedForms{};
+    for (auto intCharIt{ integralDigits.rbegin() }; intCharIt != integralDigits.rend(); ++intCharIt)
+    {
+        // treat the first element differently
+        // because for this element, we don't need to add "* 10"
+        if (intCharIt == integralDigits.rbegin())
+        {
+            // zeroes are not to be included in the expanded form
+            if (*intCharIt != '0')
+            {
+                expandedForms.push_back(std::string{ 1, *intCharIt });
+            }
+
+            // to keep the number of indentations low
+            // compared to using an 'else'
+            continue;
+        }
+
+        // zeroes are not to be included in the expanded form
+        if (*intCharIt != '0')
+        {
+            // expanded form consists of the digit, a multiplication symbol, a multiplier value and whitespaces
+            std::string expandedForm{ 1, *intCharIt };
+            expandedForm += " * ";
+            expandedForm += multiplyValue;
+
+            // insert at the beginning of the vector so printing vector contents becomes easier
+            expandedForms.insert(expandedForms.begin(), expandedForm);
+        }
+
+        // for the next digit
+        multiplyValue += '0';
+    }
+
+    // for decimal digits, the process is the same as above
+    // but we can start at the first element instead of the last
+    // since the first element's place value is the highest among
+    // the other decimal digits (we start with n / 10, then n / 100 etc.)
+    std::string divideValue{ "10" };
+    for (auto decCharIt{ decimalDigits.begin() }; decCharIt != decimalDigits.end(); ++decCharIt)
+    {
+        // zeroes not to be included
+        if (*decCharIt != '0')
+        {
+            // expanded form consists of the digit, a division symbol, the divisor and whitespaces
+            std::string expandedForm{ 1, *decCharIt };
+            expandedForm += " / ";
+            expandedForm += divideValue;
+
+            expandedForms.push_back(expandedForm);
+        }
+
+        // for the next digit
+        divideValue += '0';
+    }
+
+    std::cout << "Expanded forms:\n";
+    my_functions::printVectorToStdOut(expandedForms);
+    std::cout << '\n';
+
+    // now we need to concatenate all of them and
+    // add in '+' as necessary
+    std::string fullExpandedForm{};
+    for (const auto& str : expandedForms)
+    {
+        // do not add '+' if the element is last
+        if (str == *expandedForms.rbegin())
+        {
+            fullExpandedForm += str;
+        }
+        else
+        {
+            fullExpandedForm += str + " + ";
+        }
+    }
+
+    std::cout << sampleInput << '\n';
+    std::cout << fullExpandedForm << '\n';
+    
     return 0;
 }
