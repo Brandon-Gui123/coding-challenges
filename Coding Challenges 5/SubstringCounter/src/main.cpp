e3f73fd71f8c07adcdf722c3b9791ba5cb3197c7
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-26 11:35:06 +0800

    Add sample inputs and expected outputs to main.cpp

--- Coding Challenges 5/SubstringCounter/src/main.cpp
+++ Coding Challenges 5/SubstringCounter/src/main.cpp
@@ -1,7 +1,19 @@
+#include "my_functions.h"
+
+#include <cassert>          // for assert macro
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello there, world!\n";
+    std::string sampleFullText{ "aa_bb_cc_dd_bb_e" };
+    std::string sampleSearchText{ "bb" };
+
+    int count{ my_functions::getSubstringCount(sampleFullText, sampleSearchText) };
+
+    assert(count == 2);
+
+    std::cout << "Success!\n";
+
     return 0;
 }
