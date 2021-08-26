bc5e5792c292d6867f5c7018768aebb902578947
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-26 11:32:13 +0800

    Create my_functions.cpp file

---
+++ Coding Challenges 5/SubstringCounter/src/my_functions.cpp
@@ -0,0 +1,9 @@
+#include <string>       // for std::string
+
+namespace my_functions
+{
+    int getSubstringCount(const std::string& fullText, const std::string& substring)
+    {
+        return 0;
+    }
+}
