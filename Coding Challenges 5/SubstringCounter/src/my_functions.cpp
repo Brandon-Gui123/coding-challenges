10e190af3c77cb95b3271eb360e7a51d62bcc5f5
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-26 11:44:12 +0800

    Add implementation to getSubstringCount function

--- Coding Challenges 5/SubstringCounter/src/my_functions.cpp
+++ Coding Challenges 5/SubstringCounter/src/my_functions.cpp
@@ -4,6 +4,30 @@ namespace my_functions
 {
     int getSubstringCount(const std::string& fullText, const std::string& substring)
     {
-        return 0;
+        int numMatches{ 0 };
+        auto substringIterator{ substring.begin() };
+        
+        for (const char currentFullTextChar : fullText)
+        {
+            if (currentFullTextChar == *substringIterator)
+            {
+                // for checking if the next character also matches the full text char in
+                // the next iteration
+                ++substringIterator;
+
+                if (substringIterator == substring.end())
+                {
+                    // reaching the end of the substring means a match
+                    // has been successful
+                    ++numMatches;
+                }
+            }
+            else
+            {
+                substringIterator = substring.begin();
+            }
+        }
+
+        return numMatches;
     }
 }
