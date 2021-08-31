9c084d920582cb77f595ad55422ea353152ba179
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-31 11:54:34 +0800

    Create function to do what the challenge wants
    Except it takes in a vector instead of an array.

--- Coding Challenges 6/ArrayIntegersSumSides/src/main.cpp
+++ Coding Challenges 6/ArrayIntegersSumSides/src/main.cpp
@@ -1,4 +1,42 @@
+#include <vector>
 #include <iostream>
+
+int leftRightSumIndexEquals(const std::vector<int>& integerVector)
+{
+    int leftSum{ integerVector[0] };
+    int rightSum{ 0 };
+
+    for (int i{ 2 }; i < integerVector.size(); ++i)
+    {
+        rightSum += integerVector[i];
+    }
+
+    // sum of elements before the third element and
+    // the sum of elements after the third element are equal
+    if (leftSum == rightSum)
+    {
+        return 1;
+    }
+
+    // start at the third element because we've
+    // already calculated the sum of the left and right
+    // elements of the second element
+    for (int n{ 2 }; n < integerVector.size() - 1; ++n)
+    {
+        leftSum += integerVector[n - 1];
+        rightSum -= integerVector[n + 1];
+
+        if (leftSum == rightSum)
+        {
+            return n;
+        }
+    }
+
+    // this value will most likely not be used in array indexes
+    // so we'll use this return value to indicate that we can't
+    // find anything
+    return -1;
+}
 
 int main()
 {
