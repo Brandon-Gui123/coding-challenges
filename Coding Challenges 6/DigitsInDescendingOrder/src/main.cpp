7ef0e3dcc7f95a7897f12a79a2bfa76fe91b6e08
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-06 20:32:35 +0800

    Remove comments in asDescending function

--- Coding Challenges 6/DigitsInDescendingOrder/src/main.cpp
+++ Coding Challenges 6/DigitsInDescendingOrder/src/main.cpp
@@ -4,11 +4,6 @@
 
 int asDescending(int number)
 {
-    // 1. Store each digit in a vector by using the % and / operators
-    //      - do num % 10 to extract the rightmost digit and store it in the vector
-    //        (order doesn't matter, since we will be sorting later)
-    //      - do num / 10 to remove the rightmost digit, moving all other digits to the right by one space
-    //      - continue until input is 0
     std::vector<int> digits{};
     int numberCopy{ number };
     do
@@ -17,12 +12,8 @@ int asDescending(int number)
         numberCopy /= 10;
     } while (numberCopy != 0);
 
-    // 2. We can use std::sort to sort the numbers in descending order
-    //    To do this, we give the function a function pointer (or a lambda would work too)
     std::sort(digits.begin(), digits.end(), std::greater{});
 
-    // 3. To reconstruct the number, we can use rbegin() and rend() to iterate the vector backwards.
-    //    The digit is then multiplied by a multiplier that gets multiplied by 10 every iteration.
     int result{ 0 };
     int multiplier{ 1 };
     for (auto it{ digits.rbegin() }; it != digits.rend(); ++it)
