0b67ac9be5b7758dc12ca87206922ee35403861d
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-07 13:15:57 +0800

    Create sumBooleans function

--- Coding Challenges 6/BinarySumInHexadecimal/src/main.cpp
+++ Coding Challenges 6/BinarySumInHexadecimal/src/main.cpp
@@ -7,6 +7,26 @@ public:
     bool carryOver{};
 };
 
+SumBooleansResult sumBooleans(bool first, bool second)
+{
+    if (first ^ second)
+    {
+        // both booleans are different in value
+        // the summation will result in a '1' or a true
+        return { true, false };
+    }
+    else if (first && second)
+    {
+        // both booleans are the same in value
+        // the resulting value will be a '0' but
+        // whether there is carry over will depend on what the individual
+        // values were
+        // if the individual values were '1' (true), then there's carry over
+        // else, no carry over
+        return { false, first };
+    }
+}
+
 int main()
 {
     std::cout << "Hello there, world!\n";
