a7d1d3e050bab43c9371bd0e059b514421112c76
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-07 16:37:59 +0800

    Create sum function

--- Coding Challenges 6/BinarySumInHexadecimal/src/main.cpp
+++ Coding Challenges 6/BinarySumInHexadecimal/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>     // for std::cout
+#include <vector>       // for std::vector
 
 struct SumBooleansResult
 {
@@ -47,6 +48,63 @@ void makeEqualSize(std::vector<bool>& first, std::vector<bool>& second)
             second.insert(second.begin(), first.size() - second.size(), false);
         }
     }
+}
+
+std::vector<bool> sum(const std::vector<bool>& first, const std::vector<bool>& second)
+{
+    // we may need to modify both Boolean vectors, so let's make a copy
+    std::vector<bool> firstCopy{ first };
+    std::vector<bool> secondCopy{ second };
+
+    // ensure both vectors are of equal size
+    // this will simplify addition
+    makeEqualSize(firstCopy, secondCopy);
+
+    // for when the previous summation of Booleans resulted in a value to carry over
+    bool hasCarryOver{ false };
+    
+    for (auto firstCopyIt{ firstCopy.rbegin() }, secondCopyIt{ secondCopy.rbegin() };
+        firstCopyIt != firstCopy.rend() && secondCopyIt != secondCopy.rend();
+        ++firstCopyIt, ++secondCopyIt)
+    {
+        // sum the 2 Booleans that both iterators are pointing to
+        SumBooleansResult sumResult{ sumBooleans(*firstCopyIt, *secondCopyIt) };
+
+        // we will use the first Boolean vector to return the result
+        // so we modify that
+        *firstCopyIt = sumResult.resultingValue;
+
+        // any carry over required from summing both Booleans?
+        bool currentHasCarryOver{ sumResult.carryOver };
+
+        // for possible carry over due to summing a Boolean with a carry over
+        bool carryOverHasCarryOver{ false };
+
+        // if there's carry over from the previous sum, we will need to apply it here
+        if (hasCarryOver)
+        {
+            // sum the current Boolean with the carry over
+            SumBooleansResult carryOverSumResult{ sumBooleans(*firstCopyIt, true) };
+            *firstCopyIt = carryOverSumResult.resultingValue;
+            
+            // any carry over required from summing with a carry over?
+            carryOverHasCarryOver = carryOverSumResult.carryOver;
+        }
+
+        // we won't have times when we have 2 carry overs so we
+        // can use the XOR operator here to specify if a carry over
+        // is required for the next digit
+        hasCarryOver = currentHasCarryOver ^ carryOverHasCarryOver;
+    }
+
+    // do we still have carry over?
+    // if we have, we need to add a 1 to the first copy vector
+    if (hasCarryOver)
+    {
+        firstCopy.insert(firstCopy.begin(), true);
+    }
+
+    return firstCopy;
 }
 
 int main()
