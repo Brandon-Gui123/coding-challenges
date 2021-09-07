7363ebf52b5b349e5e2041f5d6f25b1092d14f66
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-07 13:15:00 +0800

    Create SumBooleansResult struct
    Return type for a function that I intend to make, which takes 2 booleans
    as bits and sums them both.

--- Coding Challenges 6/BinarySumInHexadecimal/src/main.cpp
+++ Coding Challenges 6/BinarySumInHexadecimal/src/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>     // for std::cout
 
+struct SumBooleansResult
+{
+public:
+    bool resultingValue{};
+    bool carryOver{};
+};
+
 int main()
 {
     std::cout << "Hello there, world!\n";
