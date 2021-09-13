d51e887307d3c59bfe0e56807c8e0958fa4e90e1
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-13 16:38:14 +0800

    Create .NET Console project
    Created via the command `dotnet new console --framework net5.0`

---
+++ Coding Challenges 7/AnagramFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnagramFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
