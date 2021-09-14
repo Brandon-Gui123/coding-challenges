6670ec46918eba0dc230eaf1b465b31968c6ae63
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-14 21:37:43 +0800

    Create .NET 5.0 console project

---
+++ Coding Challenges 7/Dashatize/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dashatize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
