24c02be40885e2e24b687a04018b335203232325
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-14 15:51:45 +0800

    Update Main to give output as shown in image

--- Coding Challenges 7/AnagramFinder/Program.cs
+++ Coding Challenges 7/AnagramFinder/Program.cs
@@ -6,81 +6,30 @@ namespace AnagramFinder
 {
     class Program
     {
-
-        static void Increment(int[] indices, int limit)
-        {
-            indices[^1]--;
-
-            for (int i = indices.Length - 1; i >= 1; i--)
-            {
-                if (indices[i] == -1)
-                {
-                    // carry over to next index
-                    indices[i - 1]--;
-
-                    // reset current
-                    indices[i] = limit;
-                }
-            }
-        }
-
         static void Main(string[] args)
         {
             string input = "baba";
 
-            // the number of indices to create
-            // depends on how many inputs we have
-            int[] indices = new int[input.Length];
+            Console.WriteLine($"anagrams(\"{input}\") =>");
 
-            for (int i = 0; i < indices.Length; i++)
+            List<string> anagrams = Anagrams.Get(input);
+            foreach (string anagram in anagrams)
             {
-                // make all indices point to the last element in the input list
-                indices[i] = input.Length - 1;
-            }
-
-            // the first index in the indices list goes from last to first in the input list
-
-            // after that index is at the first element in the list,
-            // move the next index in the indices list up by 1
-
-            // since permutations are meant to be unique, we could
-            // use a HashSet here.
-            // This will gives us O(1) time complexity when using the
-            // Contains() method
-            HashSet<string> permutations = new();
-
-            while (true)
-            {
-                string permutation = "";
-
-                // ignore permutation if 1 or more indices are equal to
-                // another
-                if (indices.Distinct().Count() == indices.Length)
+                if (anagram == anagrams.First())
                 {
-                    for (int i = 0; i < indices.Length; i++)
-                    {
-                        permutation += input[indices[i]];
-                    }
-
-                    // the Add() method will not add the permutation
-                    // if it already exists, so we don't have to check
-                    // beforehand since it already does the checking for us
-                    permutations.Add(permutation);
+                    Console.Write('[');
                 }
 
-                // next index?????? idk
-                Increment(indices, input.Length - 1);
+                Console.Write($"\"{anagram}\"");
 
-                if (indices.Any(index => index < 0))
+                if (anagram == anagrams.Last())
                 {
-                    Console.WriteLine("Terminate infinite loop");
-                    break;
+                    Console.Write(']');
                 }
-            }
-
-            foreach (string permutation in permutations)
-            {
-                Console.WriteLine(permutation);
+                else
+                {
+                    Console.Write(", ");
+                }
             }
         }
     }
