2166b6f759341d25b52ce841c1ddbb51ab78f1ff
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-14 15:40:22 +0800

    Create AdvanceIndexers in Anagrams class

--- Coding Challenges 7/AnagramFinder/Anagrams.cs
+++ Coding Challenges 7/AnagramFinder/Anagrams.cs
@@ -1,7 +1,23 @@
+using System.Collections.Generic;
 namespace AnagramFinder
 {
     public static class Anagrams
     {
-        
+        private static void AdvanceIndexers(List<int> indexers, int wordCount)
+        {
+            // this moves the last index to point at the
+            // element before what it is pointing
+            indexers[^1]--;
+
+            // to go through all other indexers, except the first one
+            for (int i = indexers.Count - 1; i >= 1; i--)
+            {
+                if (indexers[i] == -1)
+                {
+                    indexers[i - 1]--;
+                    indexers[i] = wordCount - 1;
+                }
+            }
+        }
     }
 }
