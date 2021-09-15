864ad1894b7b5a5907fc53a28d0601eadaec0d35
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-15 11:33:07 +0800

    Create private DashesPlacement class in Dashatize

--- Coding Challenges 7/Dashatize/Dashatize.cs
+++ Coding Challenges 7/Dashatize/Dashatize.cs
@@ -2,6 +2,18 @@ namespace DashatizeUtils
 {
     public static class Dashatize
     {
+        private class DashesPlacement
+        {
+            public bool HasLeftDash { get; init; }
+            public bool HasRightDash { get; init; }
+            public int Digit { get; init; }
 
+            public DashesPlacement(bool hasLeftDash, bool hasRightDash, int digit)
+            {
+                HasLeftDash = hasLeftDash;
+                HasRightDash = hasRightDash;
+                Digit = digit;
+            }
+        }
     }
 }
