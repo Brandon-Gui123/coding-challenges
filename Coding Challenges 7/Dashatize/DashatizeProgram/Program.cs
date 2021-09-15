6d69306e442abb9f1cd41ff46184dc1375b18015
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-15 15:50:26 +0800

    Update Main to give output similar to the post

--- Coding Challenges 7/Dashatize/DashatizeProgram/Program.cs
+++ Coding Challenges 7/Dashatize/DashatizeProgram/Program.cs
@@ -8,7 +8,13 @@ namespace DashatizeUtils
     {
         static void Main(string[] args)
         {
-            
+            int input1 = 274;
+            string dashatized1 = Dashatize.DashatizeOdds(input1);
+            Console.WriteLine($"dashatize({input1}) => '{dashatized1}'");
+
+            int input2 = 6814;
+            string dashatized2 = Dashatize.DashatizeOdds(input2);
+            Console.WriteLine($"dashatize({input2}) => '{dashatized2}'");
         }
     }
 }
