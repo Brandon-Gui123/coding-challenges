97e08aee7339aa335b5e6e6615d7e615aee88056
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-15 12:01:50 +0800

    Create DashatizeOdds method in Dashatize

--- Coding Challenges 7/Dashatize/Dashatize.cs
+++ Coding Challenges 7/Dashatize/Dashatize.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Text;
 
 namespace DashatizeUtils
 {
@@ -16,6 +17,58 @@ namespace DashatizeUtils
                 HasRightDash = hasRightDash;
                 Digit = digit;
             }
+        }
+    
+        public static string DashatizeOdds(int integerInput)
+        {
+            List<int> reversedDigits = IntToReversedDigits(integerInput);
+
+            // 10 is the max number of digits an integer can have
+            // since more than that means the value is bigger than int.MaxValue
+            Dictionary<int, DashesPlacement> indexAndDashes = new(10);
+
+            // go through each digit in reverse order
+            for (int i = reversedDigits.Count - 1; i >= 0; i--)
+            {
+                if (reversedDigits[i] % 2 != 0)
+                {
+                    // this odd digit shall have a left dash if
+                    // 1. it isn't the first digit
+                    // 2. the previous digit isn't odd
+                    bool hasLeftDash = (i != 0) && (i > 0 && reversedDigits[i - 1] % 2 != 1);
+
+                    // this odd digit shall have a right dash if
+                    // 1. it isn't the last digit
+                    bool hasRightDash = i < reversedDigits.Count - 1;
+
+                    DashesPlacement dashesPlacement = new(hasLeftDash, hasRightDash, reversedDigits[i]);
+                    indexAndDashes.Add(i, dashesPlacement);
+                }
+            }
+
+            // I'll be continuously building upon the string
+            // so a StringBuilder may come in handy here
+            StringBuilder dashatizedBuilder = new(reversedDigits.Count);
+
+            for (int i = reversedDigits.Count - 1; i >= 0; i--)
+            {
+                if (indexAndDashes.TryGetValue(i, out var dashesPlacement))
+                {
+                    if (dashesPlacement.HasLeftDash)
+                        dashatizedBuilder.Append('-');
+
+                    dashatizedBuilder.Append(dashesPlacement.Digit);
+
+                    if (dashesPlacement.HasRightDash)
+                        dashatizedBuilder.Append('-');
+                }
+                else
+                {
+                    dashatizedBuilder.Append(reversedDigits[i]);
+                }
+            }
+
+            return dashatizedBuilder.ToString();
         }
 
         private static List<int> IntToReversedDigits(int integer)
