5e8718849c9053ab1861f191b3c14704a8e9c963
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-15 12:48:44 +0800

    Add DashatizeWithBothDashes test

--- Coding Challenges 7/Dashatize/DashatizeUnitTests/DashatizeTests.cs
+++ Coding Challenges 7/Dashatize/DashatizeUnitTests/DashatizeTests.cs
@@ -1,9 +1,20 @@
 using NUnit.Framework;
+using DashatizeUtils;
+using FluentAssertions;
 
 namespace DashatizeUnitTests
 {
     public class Tests
     {
-        
+        [Test]
+        public void DashatizeWithBothDashes()
+        {
+            const int input = 22322;
+            string result = Dashatize.DashatizeOdds(input);
+            result.Should().Be(
+                "22-3-22",
+                because: "there is just 1 odd number and there is space for left and right dashes"
+            );
+        }
     }
 }
