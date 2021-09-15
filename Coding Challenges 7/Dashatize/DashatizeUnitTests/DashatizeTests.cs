0070070d07d1ad6862b82202e835ce0b61d5aa85
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-15 12:41:53 +0800

    Remove sample methods in test script

--- Coding Challenges 7/Dashatize/DashatizeUnitTests/DashatizeTests.cs
+++ Coding Challenges 7/Dashatize/DashatizeUnitTests/DashatizeTests.cs
@@ -4,15 +4,6 @@ namespace DashatizeUnitTests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+        
     }
 }
