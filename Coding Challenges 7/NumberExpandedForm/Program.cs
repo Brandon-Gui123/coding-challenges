19f7a1efb354fed5e7ff27bb062691c9fe9dc2f7
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-16 10:39:26 +0800

    Create .NET 5.0 console project for challenge

---
+++ Coding Challenges 7/NumberExpandedForm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumberExpandedForm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
