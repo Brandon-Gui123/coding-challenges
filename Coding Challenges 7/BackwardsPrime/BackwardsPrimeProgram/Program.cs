67f87adeaf3875aff42fb4ba265fdee6189cb1e4
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-17 21:06:35 +0800

    Create .NET 5.0 console project

---
+++ Coding Challenges 7/BackwardsPrime/BackwardsPrimeProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BackwardsPrimeProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there!");
+        }
+    }
+}
