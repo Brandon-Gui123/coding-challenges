979a441b33ca090f1531d1a57fce76270a7f2d2d
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-19 18:01:18 +0800

    Create tests for IsPrime

--- Coding Challenges 7/BackwardsPrime/BackwardsPrimeTests/IsPrimeTests.cs
+++ Coding Challenges 7/BackwardsPrime/BackwardsPrimeTests/IsPrimeTests.cs
@@ -6,6 +6,39 @@ namespace BackwardsPrimeTests
 {
     public class IsPrimeTests
     {
+        [Test]
+        public void TwoIsPrimeNumber()
+        {
+            bool result = BackwardsPrimeMethods.IsPrime(2);
+            result.Should().BeTrue(because: "2 is divisible by only 1 and itself");
+        }
 
+        [Test]
+        public void ThreeIsPrimeNumber()
+        {
+            bool result = BackwardsPrimeMethods.IsPrime(3);
+            result.Should().BeTrue(because: "3 is divisible by only 1 and itself");
+        }
+
+        [Test]
+        public void FiveIsPrimeNumber()
+        {
+            bool result = BackwardsPrimeMethods.IsPrime(5);
+            result.Should().BeTrue(because: "5 is divisible by only 1 and itself");
+        }
+
+        [Test]
+        public void FourIsNotPrimeNumber()
+        {
+            bool result = BackwardsPrimeMethods.IsPrime(4);
+            result.Should().BeFalse(because: "4 is divisible by 2");
+        }
+
+        [Test]
+        public void NineIsNotPrimeNumber()
+        {
+            bool result = BackwardsPrimeMethods.IsPrime(9);
+            result.Should().BeFalse(because: "9 is divisible by 3");
+        }
     }
 }
