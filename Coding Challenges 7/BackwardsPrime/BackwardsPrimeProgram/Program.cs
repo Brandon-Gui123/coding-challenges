d8b3ad052e6a8586c03502cde7454b04920f2410
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-19 23:07:45 +0800

    Create functions in Program class

--- Coding Challenges 7/BackwardsPrime/BackwardsPrimeProgram/Program.cs
+++ Coding Challenges 7/BackwardsPrime/BackwardsPrimeProgram/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BackwardsPrimeProgram
 {
@@ -6,7 +8,43 @@ namespace BackwardsPrimeProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello there!");
+            ShowOutputAsExampleFormat(2, 100);
+        }
+
+        static List<int> GetEmirpsFromRange(int start, int end)
+        {
+            List<int> emirps = new();
+
+            foreach (int value in Enumerable.Range(start, end))
+            {
+                if (BackwardsPrimeMethods.IsEmirp(value))
+                {
+                    emirps.Add(value);
+                }
+            }
+
+            return emirps;
+        }
+    
+        static void ShowOutputAsExampleFormat(int start, int end)
+        {
+            List<int> emirps = GetEmirpsFromRange(start, end);
+
+            Console.WriteLine($"backwardsPrime({start}, {end}) =>");
+
+            Console.Write('[');
+            for (int i = 0; i < emirps.Count; i++)
+            {
+                Console.Write(emirps[i]);
+
+                if (i != emirps.Count - 1)
+                {
+                    // add a comma and a space
+                    // for elements that aren't last
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine(']');
         }
     }
 }
