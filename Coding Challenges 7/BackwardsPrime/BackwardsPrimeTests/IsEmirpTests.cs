469fd9eaa276be7e69c3581f36f91d294a8e1cf8
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-19 22:51:32 +0800

    Create tests for IsEmirp method

--- Coding Challenges 7/BackwardsPrime/BackwardsPrimeTests/IsEmirpTests.cs
+++ Coding Challenges 7/BackwardsPrime/BackwardsPrimeTests/IsEmirpTests.cs
@@ -6,6 +6,46 @@ namespace BackwardsPrimeTests
 {
     public class IsEmirpTests
     {
-        
+        [Test]
+        public void Given13_ReturnsTrue()
+        {
+            bool result = BackwardsPrimeMethods.IsEmirp(13);
+            result.Should().BeTrue(because: "13 and 31 are prime numbers");
+        }
+
+        [Test]
+        public void Given2_ReturnsFalse()
+        {
+            bool result = BackwardsPrimeMethods.IsEmirp(2);
+            result.Should().BeFalse(because: "an emirp must have at least 2 digits");
+        }
+
+        [Test]
+        public void Given11_ReturnsFalse()
+        {
+            bool result = BackwardsPrimeMethods.IsEmirp(11);
+            result.Should().BeFalse(because: "both the number and reversed number must create unique numbers");
+        }
+
+        [Test]
+        public void Given23_ReturnsFalse()
+        {
+            bool result = BackwardsPrimeMethods.IsEmirp(23);
+            result.Should().BeFalse(because: "while 23 is a prime number, the reversed number (32) is not");
+        }
+    
+        [Test]
+        public void Given1_ReturnsFalse()
+        {
+            bool result = BackwardsPrimeMethods.IsEmirp(1);
+            result.Should().BeFalse(because: "1 is not a valid prime number");
+        }
+
+        [Test]
+        public void GivenNegative1_ReturnsFalse()
+        {
+            bool result = BackwardsPrimeMethods.IsEmirp(-1);
+            result.Should().BeFalse(because: "negative numbers are not valid prime numbers");
+        }
     }
 }
