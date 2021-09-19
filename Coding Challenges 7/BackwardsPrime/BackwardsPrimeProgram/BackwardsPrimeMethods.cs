8641f152911911ed83d696f329c6e1775a8a71e5
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-19 18:00:52 +0800

    Create IsPrime method

--- Coding Challenges 7/BackwardsPrime/BackwardsPrimeProgram/BackwardsPrimeMethods.cs
+++ Coding Challenges 7/BackwardsPrime/BackwardsPrimeProgram/BackwardsPrimeMethods.cs
@@ -2,6 +2,19 @@ namespace BackwardsPrimeProgram
 {
     public static class BackwardsPrimeMethods
     {
-        
+        public static bool IsPrime(int number)
+        {
+            // trial division used here
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
