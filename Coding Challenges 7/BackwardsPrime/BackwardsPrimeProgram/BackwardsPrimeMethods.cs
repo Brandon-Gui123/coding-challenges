8a75f359912e795f194bc478bfcbcfaaae7ec72a
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-19 18:09:30 +0800

    Add check for input < 2 in IsPrime

--- Coding Challenges 7/BackwardsPrime/BackwardsPrimeProgram/BackwardsPrimeMethods.cs
+++ Coding Challenges 7/BackwardsPrime/BackwardsPrimeProgram/BackwardsPrimeMethods.cs
@@ -4,6 +4,12 @@ namespace BackwardsPrimeProgram
     {
         public static bool IsPrime(int number)
         {
+            // numbers lesser than 2 aren't valid prime numbers
+            if (number < 2)
+            {
+                return false;
+            }
+
             // trial division used here
 
             for (int i = 2; i < number; i++)
