d43a28759e196ac723da3d2c6cd5e1c6bb9cd8b7
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-26 00:25:44 +0800

    Create camelCaseToKebabCase method

--- Coding Challenges 7/Kebabtize/Program.cs
+++ Coding Challenges 7/Kebabtize/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Kebabtize
 {
@@ -7,6 +9,49 @@ namespace Kebabtize
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static string camelCaseToKebabCase(string input)
+        {
+            // indexes where we can find patterns where a lowercase
+            // letter precedes an uppercase letter
+            // the index is at the lowercase letter
+            HashSet<int> foundSequencesIndexes = new();
+
+            // detecting lowercase letters preceding uppercase letters
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                char currentChar = input[i];
+                char nextChar = input[i + 1];
+
+                if (char.IsLower(currentChar) && char.IsUpper(nextChar))
+                {
+                    foundSequencesIndexes.Add(i);
+                }
+            }
+
+            // kebabtizing the string
+
+            // wastes less memory compared to using just strings
+            StringBuilder kebabtizedStringBuilder = new();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (foundSequencesIndexes.Contains(i))
+                {
+                    kebabtizedStringBuilder.Append(input[i])
+                                           .Append('-')
+                                           .Append(char.ToLower(input[i + 1]));
+
+                    i++;
+                }
+                else
+                {
+                    kebabtizedStringBuilder.Append(input[i]);
+                }
+            }
+
+            return kebabtizedStringBuilder.ToString();
         }
     }
 }
