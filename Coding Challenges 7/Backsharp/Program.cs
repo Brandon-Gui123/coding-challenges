6c23500d78ebe1f5aad6719872e66ea17529d916
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-25 14:22:23 +0800

    Create C# .NET 5.0 console project

---
+++ Coding Challenges 7/Backsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Backsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
