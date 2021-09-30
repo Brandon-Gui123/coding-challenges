dd9c8d07a3a5ee7f6f16ee6f6b9008c73fbc16dd
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-09-30 16:26:08 +0800

    Create members for IntSequence class

--- Coding Challenges 8/LargestDigitNumber/src/main/java/LargestDigitNumber/IntSequence.java
+++ Coding Challenges 8/LargestDigitNumber/src/main/java/LargestDigitNumber/IntSequence.java
@@ -1,5 +1,38 @@
 package LargestDigitNumber;
 
+import java.util.ArrayList;
+
 public class IntSequence
 {
+    private ArrayList<Integer> m_numbers;
+
+    public IntSequence()
+    {
+        m_numbers = new ArrayList<>();
+    }
+
+    public int get(int index)
+    {
+        return m_numbers.get(index);
+    }
+
+    public int size()
+    {
+        return m_numbers.size();
+    }
+
+    public int sum()
+    {
+        int sum = 0;
+        for (int number : m_numbers)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+
+    public void add(int number)
+    {
+        m_numbers.add(number);
+    }
 }
