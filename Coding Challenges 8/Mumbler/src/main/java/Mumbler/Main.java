9986165ffdb135e1f19d833e6cb47fb8b364068f
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-10-01 17:13:21 +0800

    Create mumble method in Main class

--- Coding Challenges 8/Mumbler/src/main/java/Mumbler/Main.java
+++ Coding Challenges 8/Mumbler/src/main/java/Mumbler/Main.java
@@ -6,4 +6,26 @@ public class Main
     {
         System.out.println("Darn");
     }
+
+    public static String mumble(String input)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < input.length(); i++)
+        {
+            char currentChar = input.charAt(i);
+            stringBuilder.append(Character.toUpperCase(currentChar));
+
+            for (int j = 0; j < i; j++)
+            {
+                stringBuilder.append(Character.toLowerCase(currentChar));
+            }
+
+            if (i < input.length() - 1)
+            {
+                stringBuilder.append('-');
+            }
+        }
+
+        return stringBuilder.toString();
+    }
 }
