65b9c00ecdad7133938f4956f923e3698f615c5a
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-10-03 02:03:31 +0800

    Update toWeirdCase to validate input string

--- Coding Challenges 8/WeirdStringCase/src/main/java/FunUtils.java
+++ Coding Challenges 8/WeirdStringCase/src/main/java/FunUtils.java
@@ -2,6 +2,15 @@ public class FunUtils
 {
     public static String toWeirdCase(String input)
     {
+        if (input == null)
+        {
+            throw new IllegalArgumentException("input cannot be null");
+        }
+        else if (StringUtils.containsNonLettersAndNonWhitespace(input))
+        {
+            throw new IllegalArgumentException("input can only contain whitespaces and letters");
+        }
+
         var resultBuilder = new StringBuilder(input.length());
 
         for (int i = 0; i < input.length(); i++)
