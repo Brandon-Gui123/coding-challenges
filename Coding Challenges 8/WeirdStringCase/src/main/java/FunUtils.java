877fa79161d733d2a37aea1326006d61e95250cd
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-10-03 01:13:58 +0800

    Create toWeirdCase method

--- Coding Challenges 8/WeirdStringCase/src/main/java/FunUtils.java
+++ Coding Challenges 8/WeirdStringCase/src/main/java/FunUtils.java
@@ -1,3 +1,23 @@
 public class FunUtils
 {
+    public static String toWeirdCase(String input)
+    {
+        var resultBuilder = new StringBuilder(input.length());
+
+        for (int i = 0; i < input.length(); i++)
+        {
+            var inputChar = input.charAt(i);
+
+            if (i % 2 == 0)
+            {
+                resultBuilder.append(Character.toUpperCase(inputChar));
+            }
+            else
+            {
+                resultBuilder.append(Character.toLowerCase(inputChar));
+            }
+        }
+
+        return resultBuilder.toString();
+    }
 }
