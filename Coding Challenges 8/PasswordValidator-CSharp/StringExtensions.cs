01adaf0d0cc6e1b1d15331a0b497dd908293aecd
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-10-04 16:09:19 +0800

    Create Should extension method

--- Coding Challenges 8/PasswordValidator-CSharp/StringExtensions.cs
+++ Coding Challenges 8/PasswordValidator-CSharp/StringExtensions.cs
@@ -2,6 +2,9 @@ namespace PasswordValidator_CSharp
 {
     public static class StringExtensions
     {
-        
+        public static PasswordValidator Should(this string str)
+        {
+            return new(str);
+        }
     }
 }
