3cee06fb96fa7310f7e2be500808d9debf086d4d
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-10-04 10:51:39 +0800

    Create incrementString method

--- Coding Challenges 8/StringIncrementer/src/main/java/StringIncrementer/Program.java
+++ Coding Challenges 8/StringIncrementer/src/main/java/StringIncrementer/Program.java
@@ -2,4 +2,18 @@ package StringIncrementer;
 
 public class Program
 {
+    public static String incrementString(String input)
+    {
+        char lastChar = input.charAt(input.length() - 1);
+        if (Character.isDigit(lastChar))
+        {
+            int lastCharAsInt = lastChar - '0';
+            lastCharAsInt++;
+            return input.substring(0, input.length() - 1) + lastCharAsInt;
+        }
+        else
+        {
+            return input + '1';
+        }
+    }
 }
