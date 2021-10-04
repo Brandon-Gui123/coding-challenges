2618aabdae302b87a050f86554431d4010398909
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-10-04 16:08:15 +0800

    Create PasswordValidator constructor

--- Coding Challenges 8/PasswordValidator-CSharp/PasswordValidator.cs
+++ Coding Challenges 8/PasswordValidator-CSharp/PasswordValidator.cs
@@ -4,5 +4,15 @@ namespace PasswordValidator_CSharp
     {
         private string m_password;
         private bool m_currentResult;
+
+        public PasswordValidator(string password)
+        {
+            m_password = password;
+
+            // so that first-time evaluations will always be
+            // executed and not short-circuited by the conditional AND
+            // operator
+            m_currentResult = true;
+        }
     }
 }
