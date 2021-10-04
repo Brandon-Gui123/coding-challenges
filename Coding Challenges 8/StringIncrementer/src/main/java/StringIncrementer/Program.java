474d4a42bf6edd356b2b296cf42cd804b25ac94e
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-10-04 10:54:54 +0800

    Update program to print some examples

--- Coding Challenges 8/StringIncrementer/src/main/java/StringIncrementer/Program.java
+++ Coding Challenges 8/StringIncrementer/src/main/java/StringIncrementer/Program.java
@@ -2,6 +2,12 @@ package StringIncrementer;
 
 public class Program
 {
+    public static void main(String[] args)
+    {
+        printIncrementStringResult("bar");
+        printIncrementStringResult("foo09");
+    }
+
     public static String incrementString(String input)
     {
         char lastChar = input.charAt(input.length() - 1);
@@ -15,5 +21,10 @@ public class Program
         {
             return input + '1';
         }
+    }
+
+    public static void printIncrementStringResult(String input)
+    {
+        System.out.printf("%s\t->\t%s%n", input, incrementString(input));
     }
 }
