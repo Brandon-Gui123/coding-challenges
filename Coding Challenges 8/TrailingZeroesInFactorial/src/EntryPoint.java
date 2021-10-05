8822ceb2b4d21bc220380f9640de87403745eba9
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-10-05 12:56:23 +0800

    Create method to get trailing zeroes in factorial

--- Coding Challenges 8/TrailingZeroesInFactorial/src/EntryPoint.java
+++ Coding Challenges 8/TrailingZeroesInFactorial/src/EntryPoint.java
@@ -2,6 +2,11 @@ public class EntryPoint
 {
     public static void main(String[] args)
     {
-        System.out.println("Haven't I done this before?");
+        System.out.println(getTrailingZeroesCountInFactorialOf(12));
+    }
+
+    public static int getTrailingZeroesCountInFactorialOf(int num)
+    {
+        return num / 5;
     }
 }
