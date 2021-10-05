92116db09acd7ad9b03d3e066d980e51b349ea26
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-10-05 12:31:54 +0800

    Create methods to validate each criterion

--- Coding Challenges 8/PasswordValidator/src/main/java/PasswordValidator/PasswordValidation.java
+++ Coding Challenges 8/PasswordValidator/src/main/java/PasswordValidator/PasswordValidation.java
@@ -2,4 +2,50 @@ package PasswordValidator;
 
 public class PasswordValidation
 {
+    public static boolean isLengthAtLeast(String password, int minLength)
+    {
+        return password.length() >= minLength;
+    }
+
+    public static boolean containsLowercase(String password)
+    {
+        for (int i = 0; i < password.length(); i++)
+        {
+            var currentChar = password.charAt(i);
+            if (Character.isLowerCase(currentChar))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean containsUppercase(String password)
+    {
+        for (int i = 0; i < password.length(); i++)
+        {
+            var currentChar = password.charAt(i);
+            if (Character.isUpperCase(currentChar))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean containsDigit(String password)
+    {
+        for (int i = 0; i < password.length(); i++)
+        {
+            var currentChar = password.charAt(i);
+            if (Character.isDigit(currentChar))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
