e0f0c6b15ee7e13c21eb458be911ddeab733d503
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-10-05 12:40:55 +0800

    Update EntryPoint to print examples

--- Coding Challenges 8/PasswordValidator/src/main/java/PasswordValidator/EntryPoint.java
+++ Coding Challenges 8/PasswordValidator/src/main/java/PasswordValidator/EntryPoint.java
@@ -4,6 +4,16 @@ public class EntryPoint
 {
     public static void main(String[] args)
     {
-        System.out.println("I can validate that for you.");
+        printValidationResults("4fdg5Fj3");
+        printValidationResults("dsF43");
+    }
+
+    public static void printValidationResults(String inputPassword)
+    {
+        System.out.printf(
+                "%s\t->\t%s%n",
+                inputPassword,
+                PasswordValidation.isValidPassword(inputPassword)
+        );
     }
 }
