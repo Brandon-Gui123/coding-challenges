4003a015d092051dc1b6af431f556ab0764616c9
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-12-19 00:17:02 +0800

    Update program to convert hardcoded input to dec

--- app-ideas/tier-1/Bin2Dec/Program.cs
+++ app-ideas/tier-1/Bin2Dec/Program.cs
@@ -6,7 +6,21 @@ namespace Bin2Dec
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string binaryInput = "11111111";
+
+            var sum = 0;
+
+            for (int i = binaryInput.Length - 1; i >= 0; i--)
+            {
+                var digit = (int)char.GetNumericValue(binaryInput[i]);
+
+                if (digit == 1)
+                {
+                    sum += (int)Math.Pow(2, binaryInput.Length - 1 - i);
+                }
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
